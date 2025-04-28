2ed443ab9b5dde47b057467378eb73885d8cc46a
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-04-28 20:53:21 +0200

    My solution to title case

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,27 @@
-function titleCase() {}
+function titleCase(word) {
+  // Check if the input is a string
+  if (typeof word !== "string") {
+    throw new Error("Invalid input: word must be a string.");
+  }
+
+  // Split the word into an array of characters
+  const characters = word.split("");
+
+  // Initialize an empty string to hold the title-cased word
+  let titleCasedWord = "";
+
+  // Loop through each character in the array
+  for (let i = 0; i < characters.length; i++) {
+    // If it's the first character or the previous character is a space, capitalize it
+    if (i === 0 || characters[i - 1] === " ") {
+      titleCasedWord += characters[i].toUpperCase();
+    } else {
+      titleCasedWord += characters[i].toLowerCase();
+    }
+  }
+
+  // Return the title-cased word
+  return titleCasedWord;
+}
 
 module.exports = titleCase;

4a6e7d0654a09d1f1646dcb944fd78094459575d
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-04-28 22:10:56 +0200

    My solution to Count vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,21 @@
-function countVowels() {}
+function countVowels(str) {
+  const formattedStr = str.toLowerCase();
+
+  let count = 0;
+
+  for (let i = 0; i < formattedStr.length; i++) {
+    if (
+      formattedStr[i] === "a" ||
+      formattedStr[i] === "e" ||
+      formattedStr[i] === "i" ||
+      formattedStr[i] === "o" ||
+      formattedStr[i] === "u"
+    ) {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 module.exports = countVowels;

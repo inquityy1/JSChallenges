7e62f90e9721e0302d9465340cf379809fe0845d
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-04-29 13:28:42 +0200

    My solutions to array intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,27 @@
-function arrayIntersection() {}
+// function arrayIntersection(arr1, arr2) {
+//   const intersectionArr = [];
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     for (let k = 0; k < arr2.length; k++) {
+//       if (arr1[i] === arr2[k] && !intersectionArr.includes(arr2[k])) {
+//         intersectionArr.push(arr2[k]);
+//       }
+//     }
+//   }
+
+//   return intersectionArr;
+// }
+
+function arrayIntersection(arr1, arr2) {
+  const intersectionArr = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i]) && !intersectionArr.includes(arr1[i])) {
+      intersectionArr.push(arr1[i]);
+    }
+  }
+
+  return intersectionArr;
+}
 
 module.exports = arrayIntersection;

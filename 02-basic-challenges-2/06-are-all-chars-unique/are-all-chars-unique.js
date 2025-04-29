5e379b75f719874b486fde8a5d6d14724159454c
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-04-29 17:42:44 +0200

    My solution to are all chars unique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,33 @@
-function areAllCharactersUnique() {}
+// function areAllCharactersUnique(string) {
+//   const charCount = {};
+
+//   for (let i = 0; i < string.length; i++) {
+//     const char = string[i];
+
+//     if (charCount[char]) {
+//       return false;
+//     }
+
+//     charCount[char] = true;
+//   }
+
+//   return true;
+// }
+
+function areAllCharactersUnique(string) {
+  const charSet = new Set();
+
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
+
+    if (charSet.has(char)) {
+      return false;
+    }
+
+    charSet.add(char);
+  }
+
+  return true;
+}
 
 module.exports = areAllCharactersUnique;

b8edffecbacd402b0418398ff742d38fc3089979
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-04-30 17:49:07 +0200

    My solution to hashtag-generator

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,30 @@
-function generateHashtag() {}
+// function generateHashtag(str) {
+//   if (str.length > 140 || str.trim() === "") return false;
+
+//   const words = str.trim().split(/\s+/);
+
+//   const capitalizedWords = words.map(
+//     (word) => word.charAt(0).toUpperCase() + word.slice(1)
+//   );
+
+//   const hashtag = "#" + capitalizedWords.join("");
+
+//   return hashtag;
+// }
+
+function generateHashtag(str) {
+  if (str.length > 140 || str.trim() === "") {
+    return false;
+  }
+
+  const hashtag = str
+    .split(" ")
+    .reduce(
+      (tag, word) => tag + word.charAt(0).toUpperCase() + word.substring(1),
+      "#"
+    );
+
+  return hashtag;
+}
 
 module.exports = generateHashtag;

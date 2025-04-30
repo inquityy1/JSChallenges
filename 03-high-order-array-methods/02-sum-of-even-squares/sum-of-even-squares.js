b22e4a6964169cb09fd13ba755363ec8161b44bc
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-04-30 13:11:54 +0200

    My solution to sum-of-even-squares

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,9 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  const evenNumbers = numbers.filter((num) => num % 2 === 0);
+  const squaredNumbers = evenNumbers.map((num) => num * num);
+  const sumNumbers = squaredNumbers.reduce((total, num) => total + num, 0);
+
+  return sumNumbers;
+}
 
 module.exports = sumOfEvenSquares;

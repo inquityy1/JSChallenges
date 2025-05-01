ff8811e38583ca350fce0da28eb8e80b8b7071cc
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-05-01 14:29:05 +0200

    My solution to reverse-string-recursion

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,11 @@
-function reverseString() {}
+function reverseString(string) {
+  if (string === "") {
+    return "";
+  }
+
+  console.log(string.substr(1));
+
+  return reverseString(string.substr(1)) + string[0];
+}
 
 module.exports = reverseString;

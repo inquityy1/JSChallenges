44faa7f42d4826554441f7eb35f84110de704b27
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-05-01 17:41:57 +0200

    My solution to power

--- 04-recursion/06-power/power.js
+++ 04-recursion/06-power/power.js
@@ -1,3 +1,17 @@
-function power() {}
+function power(base, exponent) {
+  if (exponent === 0) {
+    return 1;
+  }
+
+  return base * power(base, exponent - 1);
+}
 
 module.exports = power;
+
+// Math perspective of this function !
+// power(2, 4) =
+// 2 * power(2, 3) =
+// 2 * 2 * power(2, 2) =
+// 2 * 2 * 2 * power(2, 1) =
+// 2 * 2 * 2 * 2 * 1
+// 16

1a065a89e122471b8dd5dc5a40e98b38af48ac99
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-05-01 17:55:12 +0200

    My solution for array-sum

--- 04-recursion/07-array-sum/array-sum-run.js
+++ 04-recursion/07-array-sum/array-sum-run.js
@@ -1,5 +1,5 @@
-const arraySum = require('./array-sum');
+const arraySum = require("./array-sum");
 
-const result = arraySum([1, 2, 3, 4, 5]);
+const result = arraySum([1, 2, 3, 4, 5, 6]);
 
 console.log(result);

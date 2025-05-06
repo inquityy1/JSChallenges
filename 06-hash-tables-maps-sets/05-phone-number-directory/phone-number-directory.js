faac3eb5eb56d23f2aa52083083bcae0e2c1ffd5
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-05-06 14:12:15 +0200

    My solution for phone-number-directory

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,13 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(arr) {
+  const contacts = new Map();
+
+  for (const contact of arr) {
+    const [key, value] = contact.split(":");
+
+    contacts.set(key, value);
+  }
+
+  return contacts;
+}
 
 module.exports = phoneNumberDirectory;

5b70557b8c5afe67cb0aeeb906b6bfa1b2e0f847
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-05-06 18:11:17 +0200

    Creating custom hash table part 1

--- 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
+++ 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
@@ -1,1 +1,8 @@
-const HashTable = require('./custom-hash-table');
+const HashTable = require("./custom-hash-table");
+
+const myHashTable = new HashTable();
+
+myHashTable.set("Jhon", "555-234-3455");
+myHashTable.set("James", "555-349-3948");
+myHashTable.set("Sara", "555-496-3334");
+myHashTable.printTable();

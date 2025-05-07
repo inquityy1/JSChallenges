ba1852d30e2dc39fe26455d45f21a6918623f097
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-05-07 16:14:53 +0200

    Fast and slow pointers

---
+++ 07-stacks-queues-linked-lists/12-fast-and-slow-pointers/fast-slow-pointers.js
@@ -0,0 +1,21 @@
+// let a = 1;
+// let b = a;
+// a = 2;
+
+// let c = { name: "Jhon" };
+// let d = c;
+// c.name = "Brad";
+
+// console.log(d.name);
+
+// function findMiddle(head) {
+//   let slow = head;
+//   let fast = head;
+
+//   while (fast !== null && fast.next !== null) {
+//     fast = fast.next.next; - fast pointer goes 2 steps
+//     slow = slow.next; - slow pointer goes only 1 step
+//   }
+
+//   return slow;
+// }

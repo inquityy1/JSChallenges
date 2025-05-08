20f7a6f20ab346e7a888aab18c34211682e02eec
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-05-08 20:49:46 +0200

    My solution to graph-depth-first

--- 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first-run.js
+++ 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first-run.js
@@ -1,5 +1,5 @@
-const Graph = require('./graph');
-const depthFirstTraversal = require('./graph-depth-first');
+const Graph = require("./graph");
+const depthFirstTraversal = require("./graph-depth-first");
 
 // Example Graph
 // A --- B
@@ -13,22 +13,22 @@ const depthFirstTraversal = require('./graph-depth-first');
 const graph = new Graph();
 
 // Add vertices
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
-graph.addVertex('E');
-graph.addVertex('F');
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
 
 // Add edges
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'C');
-graph.addEdge('B', 'D');
-graph.addEdge('C', 'D');
-graph.addEdge('C', 'E');
-graph.addEdge('D', 'F');
-graph.addEdge('E', 'F');
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "D");
+graph.addEdge("C", "E");
+graph.addEdge("D", "F");
+graph.addEdge("E", "F");
 
 graph.printAdjacencyList();
 
-console.log(depthFirstTraversal(graph, 'A'));
+console.log(depthFirstTraversal(graph, "A"));

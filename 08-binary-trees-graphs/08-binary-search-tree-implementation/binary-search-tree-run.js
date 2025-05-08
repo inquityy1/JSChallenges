d2582d0c64f904e6d073e502d7a11c33d2f990ea
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-05-08 15:18:20 +0200

    My solution for binary-search-tree

--- 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree-run.js
+++ 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree-run.js
@@ -1,4 +1,4 @@
-const { Node, BinarySearchTree } = require('./binary-search-tree');
+const { Node, BinarySearchTree } = require("./binary-search-tree");
 
 // Create a binary search tree:
 //       10
@@ -7,3 +7,11 @@ const { Node, BinarySearchTree } = require('./binary-search-tree');
 //    /
 //   2
 
+const bst = new BinarySearchTree();
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(2);
+
+bst.printTree();
--- 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree.js
+++ 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree.js
@@ -1,5 +1,54 @@
-class Node {}
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-class BinarySearchTree {}
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+
+  printTree() {
+    const printNode = (node) => {
+      if (node === null) return;
+
+      printNode(node.left);
+      console.log(node.value);
+      printNode(node.right);
+    };
+
+    printNode(this.root);
+  }
+}
 
 module.exports = { Node, BinarySearchTree };

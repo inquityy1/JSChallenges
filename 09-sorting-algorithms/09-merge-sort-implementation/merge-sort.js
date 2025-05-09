ad33b3d760307f234825284312dbd9b265a73a26
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-05-09 15:28:38 +0200

    My solution for quick-sort

--- 09-sorting-algorithms/09-merge-sort-implementation/merge-sort.js
+++ 09-sorting-algorithms/09-merge-sort-implementation/merge-sort.js
@@ -1,15 +1,3 @@
-function mergeSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
-
-  const mid = Math.floor(arr.length / 2);
-  const left = mergeSort(arr.slice(0, mid));
-  const right = mergeSort(arr.slice(mid));
-
-  return merge(left, right);
-}
-
 function merge(left, right) {
   const merged = [];
 
@@ -27,6 +15,18 @@ function merge(left, right) {
   }
 
   return merged.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
 }
 
 module.exports = mergeSort;
